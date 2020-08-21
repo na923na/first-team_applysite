00c73d92290f51a0cfee1f6ae0ffc6beca3301cf
na923na <nanb_v@naver.com>
2020-08-21 15:15:16 +0900

    asdf

--- settings.py
+++ settings.py
@@ -21,11 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '%9*3zbn07828)y8%$ls_1(s&e2gkbrnn$g8noux4vk0+owdd3l'
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+#뒤가 원래의 시크릿 키라고 하면, 문자열 한번 더 암호호
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
